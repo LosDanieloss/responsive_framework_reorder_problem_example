4a8e32fd390d1e937d82de8b76eb816f89230663
Daniel Łojewski <daniellojewski@Daniels-MacBook-Air.local>
2025-07-23 12:43:15 +0200

    chore: init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/reorder/problem/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.reorder_problem_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:reorder_problem_example/app/app.dart';
+import 'package:reorder_problem_example/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
